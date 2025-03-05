206beaaff11152b31350507fa0a74424a88b2010
Mayank2335 <SHARMAYANK2056@GMAIL.COM>
2025-03-05 10:44:41 +0530

    done

---
+++ src/home.jsx
@@ -0,0 +1,14 @@
+
+function Home(){
+    return(
+        <>
+            <h1>My name is Mayank Sharma</h1>
+            <h2>Email-sharmayank2335@gmail.com
+                <br/>
+                contact no. 971878xxxx
+            </h2>
+        </>
+        
+    )
+}
+export default Home
